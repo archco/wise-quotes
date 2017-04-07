f82e170dfadd081ef9c5a587dc91ed815afb1bf2
archco <archcoster@gmail.com>
2017-04-07 18:17:57 +0900

    Update test file

--- test/wise-quotes.test.js
+++ test/wise-quotes.test.js
@@ -3,25 +3,46 @@ const chaiAsPromiese = require('chai-as-promised');
 chai.use(chaiAsPromiese);
 const should = chai.should();
 
+// test-unit
 const WiseQuotes = require('../src/wise-quotes.js');
-const wq = new WiseQuotes({
-  database: ":memory:"
-});
 
-before(function (done) {
-  wq.migration()
-  .then(() => {
-    return wq.feed();
-  }, done)
-  .then(() => {
-    done();
-  }, done);
+let sampleFile = '../db/sample.sqlite3';
+let memory = ':memory:';
+const wq = new WiseQuotes({
+  database: sampleFile
 });
 
 describe('WiseQuotes', function () {
-  
+  before(function (done) {
+    let initialize = async () => {
+      /*// initialize.
+      let result;
+      result = await wq.migration();
+      console.log(result);
+      result = await wq.feed();
+      console.log(result);*/
+      // status.
+      let count = await wq.count;
+      console.log(`count: ${count}`);
+
+      return 'initialized';
+    };
+
+    initialize()
+    .then(r => {
+      console.log(r);
+      done();
+    })
+    .catch(err => {
+      throw err;
+    });
+  });
+
   describe('#count', function () {
     let promise = wq.count;
+    promise.catch((err) => {
+      throw err;
+    });
 
     it('should be fullfilled', function () {
       promise.should.be.fullfilled;
@@ -33,7 +54,9 @@ describe('WiseQuotes', function () {
 
   describe('#read', function () {
     let promise = wq.read(1);
-    promise.then((row) => {console.log(row);});
+    promise.catch((err) => {
+      throw err;
+    });
 
     it('should be fullfilled', function () {
       promise.should.be.fullfilled;
