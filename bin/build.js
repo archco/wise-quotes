476a36531d54add7e92e56b3ee365f847f3f3dd1
archco <archcoster@gmail.com>
2017-04-09 15:02:40 +0900

    Add quotes.json

---
+++ bin/build.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const jsonfile = require('jsonfile');
+const WiseQuotes = require('../index.js');
+
+let wq = new WiseQuotes();
+let file = path.resolve(__dirname, '../db/quotes.json');
+
+(async () => {
+  let rows = await wq.all();
+  
+  jsonfile.writeFile(file, rows, {spaces: 2}, function (err) {
+    if (err) {
+      console.error(err);  
+    } else {
+      console.log(`Generate File: ${file}`);
+    }
+  });
+})();
