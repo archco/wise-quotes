89bcc2b775ae374bfc9fe03857579669ebed8629
archco <archcoster@gmail.com>
2017-04-09 23:00:31 +0900

    Add wise-quotes-client.js #10

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const WiseQuotes = require('./src/wise-quotes.js');
+const WiseQuotesClient = require('./src/wise-quotes-client.js');
 
 module.exports = WiseQuotes;
+module.exports.client = WiseQuotesClient;
