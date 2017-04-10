01edcea0b0d381b1d3d3320c2eaa9c1745a6c378
archco <archcoster@gmail.com>
2017-04-10 13:11:59 +0900

    Implement CLI #7

--- bin/feed.js
+++
@@ -1,8 +0,0 @@
-const WiseQuotes = require('../index.js');
-
-let wq = new WiseQuotes();
-
-(async () => {
-  let result = await wq.feed();
-  console.log(result);
-})();
---
+++ bin/wise-quotes.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const jsonfile = require('jsonfile');
+const program = require('commander');
+const dateformat = require('dateformat');
+const WiseQuotes = require('../index.js');
+
+const wq = new WiseQuotes();
+
+/************************************************************
+  Definitions.
+*************************************************************/
+
+program.version('0.1.1')
+  .option('-s, --status', 'display status');
+
+program
+  .command('backup')
+  .description('generate backup.json file.')
+  .action(generateBackupJSON);
+
+program
+  .command('build')
+  .description('generate quotes.json file.')
+  .action(generateQuotesJSON);
+
+program
+  .command('feed')
+  .description('feed to database.')
+  .action(feedProcess);
+
+program.parse(process.argv);
+
+/************************************************************
+  Actions
+*************************************************************/
+
+if (process.argv.length < 3) {
+  // no arg -> help
+  program.outputHelp();
+}
+
+if (program.status) {
+  displayStatus();
+}
+
+async function displayStatus() {
+  let count = await wq.count;
+  console.log(`Total rows: ${count}`);
+
+  console.log(await wq.random);
+}
+
+async function generateQuotesJSON() {
+  let rows = await wq.all();
+  let file = path.resolve(__dirname, '../db/quotes.json');
+  
+  jsonfile.writeFile(file, rows, {spaces: 0}, function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Generate File: ${file}`);
+    }
+  });
+}
+
+async function generateBackupJSON() {
+  let rows = await wq.all();
+  let file = path.resolve(__dirname, '../db/backup/' + dateformat(new Date(), 'yyyymmdd') + '_quotes.json');
+  
+  jsonfile.writeFile(file, rows, {spaces: 2}, function (err) {
+    if (err) {
+      console.error(err);  
+    } else {
+      console.log(`Backup to: ${file}`);
+    }
+  });
+}
+
+async function feedProcess() {
+  let result = await wq.feed();
+  console.log(result);
+}
